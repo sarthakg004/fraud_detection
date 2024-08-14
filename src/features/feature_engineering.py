359abc1173bb4230d65644c16a25838dd3605f67
sarthakg004 <saarthak.gupta.mec22@itbhu.ac.in>
2024-08-14 22:33:26 +0530

    added experiment tracking using mlflow

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -1,155 +1,122 @@
 import pandas as pd
 import yaml
 import numpy as np
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif, RFECV, SequentialFeatureSelector
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
-from sklearn.feature_selection import RFECV
-from sklearn.feature_selection import SequentialFeatureSelector
+import mlflow
+import mlflow.sklearn
 
-'''Loading parameters'''
-paths  =yaml.safe_load(open('./params.yaml'))['paths']
-params  =yaml.safe_load(open('./params.yaml'))['feature_engineering']
+# Start MLflow run
+with mlflow.start_run(run_name="feature_engineering"):
 
-TRAIN_DATA_PATH = paths['train_data_cleaned_path']
-VALIDATION_DATA_PATH = paths['test_data_cleaned_path']
-TRAIN_DATA_PROCESSED_PATH = paths['train_data_processed_path']
-VALIDATION_DATA_PROCESSED_PATH = paths['test_data_processed_path']
-VARIANCE_THRESHOLD = params['variance_threshold']
-SELECTION_TECHNIQUE = params['selection_technique']
-RANDOM_STATE = params['random_state']
-KFOLDS = params['k_folds']
-REFCV_RF_STEP = params['rfecv_rf_step']
-REFCV_RF_SCORING = params['rfecv_rf_scoring']
-REFCV_XGB_EVAL_METRIC = params['rfecv_xgb_eval_metric']
-REFCV_XGB_STEP = params['rfecv_xgb_step']
-REFCV_XGB_SCORING = params['rfecv_xgb_scoring']
-ANOVA_K = params['anova_k']
-ANOVA_FIND_K = params['anova_find_k']
-SFS_DIRECTION = params['sfs_direction']
-FORWARD_SELECTION_CLASSIFIER = params['forward_selection_classifier']
-FORWARD_SELECTION_DEVICE = params['forward_selection_device']
-FORWARD_SELECTION_N_FEATURES = params['forward_selection_n_features']
-FORWARD_SELECTION_SCORING = params['forward_selection_scoring']
-FORWARD_SELECTION_CV = params['forward_selection_cv']
-FORWARD_SELECTION_XGB_EVAL_METRIC = params['forward_selection_xgb_eval_metric']
+    '''Loading parameters'''
+    paths = yaml.safe_load(open('./params.yaml'))['paths']
+    params = yaml.safe_load(open('./params.yaml'))['feature_engineering']
 
-'''Loading data'''
-train_df = pd.read_csv(TRAIN_DATA_PATH)
-validation_df = pd.read_csv(VALIDATION_DATA_PATH)
+    # Log parameters
+    mlflow.log_params(params)
 
-X = train_df.drop(columns='Target')
-y = train_df['Target']
+    TRAIN_DATA_PATH = paths['train_data_cleaned_path']
+    VALIDATION_DATA_PATH = paths['test_data_cleaned_path']
+    TRAIN_DATA_PROCESSED_PATH = paths['train_data_processed_path']
+    VALIDATION_DATA_PROCESSED_PATH = paths['test_data_processed_path']
+    VARIANCE_THRESHOLD = params['variance_threshold']
+    SELECTION_TECHNIQUE = params['selection_technique']
+    RANDOM_STATE = params['random_state']
+    KFOLDS = params['k_folds']
+    RFECV_RF_STEP = params['rfecv_rf_step']
+    RFECV_RF_SCORING = params['rfecv_rf_scoring']
+    RFECV_XGB_EVAL_METRIC = params['rfecv_xgb_eval_metric']
+    RFECV_XGB_STEP = params['rfecv_xgb_step']
+    RFECV_XGB_SCORING = params['rfecv_xgb_scoring']
+    ANOVA_K = params['anova_k']
+    ANOVA_FIND_K = params['anova_find_k']
+    SFS_DIRECTION = params['sfs_direction']
+    FORWARD_SELECTION_CLASSIFIER = params['forward_selection_classifier']
+    FORWARD_SELECTION_DEVICE = params['forward_selection_device']
+    FORWARD_SELECTION_N_FEATURES = params['forward_selection_n_features']
+    FORWARD_SELECTION_SCORING = params['forward_selection_scoring']
+    FORWARD_SELECTION_CV = params['forward_selection_cv']
+    FORWARD_SELECTION_XGB_EVAL_METRIC = params['forward_selection_xgb_eval_metric']
 
+    '''Loading data'''
+    train_df = pd.read_csv(TRAIN_DATA_PATH)
+    validation_df = pd.read_csv(VALIDATION_DATA_PATH)
 
-'''Removing constant features'''
-# Initialize VarianceThreshold with a threshold of 0 to remove constant features
-selector = VarianceThreshold(threshold=VARIANCE_THRESHOLD)
+    X = train_df.drop(columns='Target')
+    y = train_df['Target']
 
-selector.fit_transform(X)
-
-# If you want to keep the selected feature names:
-selected_features = X.columns[selector.get_support(indices=True)]
-X = X[selected_features]
-validation_df = validation_df[selected_features]
-
-
-'''Performing feature selection based on selection technique'''
-if SELECTION_TECHNIQUE == 'ANOVA':
-    if ANOVA_FIND_K : 
-
-        # Initialize variables
-        k_values = list(range(1, X.shape[1] + 1))  # Testing all possible values of k
-        mean_scores = []
-
-        # Perform cross-validation for each k value
-        for k in k_values:
-            selector = SelectKBest(score_func=f_classif, k=k)
-            X_new = selector.fit_transform(X, y)
-            # Initialize classifier (e.g., Random Forest)
-            clf = RandomForestClassifier(random_state=42)
-            # Perform cross-validation
-            cv = StratifiedKFold(n_splits=5)
-            scores = cross_val_score(clf, X_new, y, cv=cv, scoring='f1_macro')  # or any other metric
-            mean_scores.append(np.mean(scores))
-        # Find the optimal number of features
-        optimal_k = k_values[np.argmax(mean_scores)]
-        selector = SelectKBest(score_func=f_classif, k=optimal_k).fit(X, y)
-        selected_features = X.columns[selector.get_support()]
-
-    else:
-        selector = SelectKBest(score_func=f_classif, k=ANOVA_K).fit(X, y)
-        selected_features = X.columns[selector.get_support()]
-    
+    '''Removing constant features'''
+    selector = VarianceThreshold(threshold=VARIANCE_THRESHOLD)
+    selector.fit_transform(X)
+    selected_features = X.columns[selector.get_support(indices=True)]
     X = X[selected_features]
-    validation_df = validation_df[selected_features]   
+    validation_df = validation_df[selected_features]
 
-elif SELECTION_TECHNIQUE == 'RFECV_RF':
-    rf = RandomForestClassifier(random_state=RANDOM_STATE)
+    '''Performing feature selection based on selection technique'''
+    if SELECTION_TECHNIQUE == 'ANOVA':
+        if ANOVA_FIND_K:
+            k_values = list(range(1, X.shape[1] + 1))
+            mean_scores = []
+            for k in k_values:
+                selector = SelectKBest(score_func=f_classif, k=k)
+                X_new = selector.fit_transform(X, y)
+                clf = RandomForestClassifier(random_state=42)
+                cv = StratifiedKFold(n_splits=5)
+                scores = cross_val_score(clf, X_new, y, cv=cv, scoring='f1_macro')
+                mean_scores.append(np.mean(scores))
+            optimal_k = k_values[np.argmax(mean_scores)]
+            selector = SelectKBest(score_func=f_classif, k=optimal_k).fit(X, y)
+            selected_features = X.columns[selector.get_support()]
+            mlflow.log_metric("optimal_k", optimal_k)
+        else:
+            selector = SelectKBest(score_func=f_classif, k=ANOVA_K).fit(X, y)
+            selected_features = X.columns[selector.get_support()]
+        
 
-    # Initialize RFECV with cross-validation
-    rfecv_rf = RFECV(estimator=rf, step=REFCV_RF_STEP, cv=StratifiedKFold(KFOLDS), scoring=REFCV_RF_SCORING)
+    elif SELECTION_TECHNIQUE == 'RFECV_RF':
+        rf = RandomForestClassifier(random_state=RANDOM_STATE)
+        rfecv_rf = RFECV(estimator=rf, step=RFECV_RF_STEP, cv=StratifiedKFold(KFOLDS), scoring=RFECV_RF_SCORING)
+        rfecv_rf.fit(X, y)
+        selected_features_rf = X.columns[rfecv_rf.support_]
+        X = X[selected_features_rf]
+        validation_df = validation_df[selected_features_rf]
+        optimal_features = rfecv_rf.n_features_
+        mlflow.log_metric("optimal_features_rf", optimal_features)
+        
 
-    # Fit RFECV
-    rfecv_rf.fit(X, y)
+    elif SELECTION_TECHNIQUE == 'RFECV_XGB':
+        xgb = XGBClassifier(random_state=RANDOM_STATE, eval_metric=RFECV_XGB_EVAL_METRIC)
+        rfecv_xgb = RFECV(estimator=xgb, step=RFECV_XGB_STEP, cv=StratifiedKFold(KFOLDS), scoring=RFECV_XGB_SCORING)
+        rfecv_xgb.fit(X, y)
+        selected_features_xgb = X.columns[rfecv_xgb.support_]
+        X = X[selected_features_xgb]
+        validation_df = validation_df[selected_features_xgb]
+        optimal_features = rfecv_xgb.n_features_
+        mlflow.log_metric("optimal_features_xgb", optimal_features)
+        
 
-    selected_features_rf = X.columns[rfecv_rf.support_]
-    X = X[selected_features_rf]
-    validation_df = validation_df[selected_features_rf]
-    
-        # Optimal number of features
-    optimal_features = rfecv_rf.n_features_
-    print(f'Optimal number of features: {optimal_features}')
+    elif SELECTION_TECHNIQUE == 'FORWARD_SELECTION':
+        clf = {"RF": RandomForestClassifier(random_state=RANDOM_STATE, n_jobs=-1),
+               "XGB": XGBClassifier(random_state=RANDOM_STATE, device=FORWARD_SELECTION_DEVICE, eval_metric=FORWARD_SELECTION_XGB_EVAL_METRIC)}[FORWARD_SELECTION_CLASSIFIER]
+        sfs = SequentialFeatureSelector(clf, n_features_to_select=FORWARD_SELECTION_N_FEATURES, direction=SFS_DIRECTION,
+                                        scoring=FORWARD_SELECTION_SCORING, cv=FORWARD_SELECTION_CV, n_jobs=-1)
+        sfs.fit(X, y)
+        selected_features_sfs = X.columns[sfs.get_support()]
+        X = X[selected_features_sfs]
+        validation_df = validation_df[selected_features_sfs]
+        mlflow.log_metric("optimal_features_sfs", sfs.n_features_in_)
+        
 
-elif SELECTION_TECHNIQUE == 'RFECV_XGB':
-    # Initialize XGBClassifier
-    xgb = XGBClassifier(random_state=RANDOM_STATE, eval_metric=REFCV_XGB_EVAL_METRIC)
+    '''Saving processed data'''
+    train_df = pd.concat([X, y], axis=1)
+    train_df.to_csv(TRAIN_DATA_PROCESSED_PATH, index=False)
+    validation_df.to_csv(VALIDATION_DATA_PROCESSED_PATH, index=False)
 
-    # Initialize RFECV with cross-validation
-    rfecv_xgb = RFECV(estimator=xgb, step=REFCV_XGB_STEP, cv=StratifiedKFold(KFOLDS), scoring=REFCV_RF_SCORING)
-    # Fit RFECV
-    rfecv_xgb.fit(X, y)
-    
-    selected_features_xgb = X.columns[rfecv_xgb.support_]
-    X = X[selected_features_xgb]
-    validation_df = validation_df[selected_features_xgb]
-    
-    # Optimal number of features
-    optimal_features = rfecv_xgb.n_features_
-    print(f'Optimal number of features: {optimal_features}')
+    # Log the processed datasets as artifacts
+    mlflow.log_artifact(TRAIN_DATA_PROCESSED_PATH, artifact_path="processed_data")
+    mlflow.log_artifact(VALIDATION_DATA_PROCESSED_PATH, artifact_path="processed_data") 
 
-
-elif SELECTION_TECHNIQUE == 'FORWARD_SELECTION':
-    # Initialize classifier
-    clf = {"RF": RandomForestClassifier(random_state=RANDOM_STATE,n_jobs=-1),    
-           "XGB": XGBClassifier(random_state=RANDOM_STATE,device = FORWARD_SELECTION_DEVICE,eval_metric=FORWARD_SELECTION_XGB_EVAL_METRIC)}[FORWARD_SELECTION_CLASSIFIER]
-
-    # Initialize SequentialFeatureSelector
-    sfs = SequentialFeatureSelector(
-        clf,
-        n_features_to_select=FORWARD_SELECTION_N_FEATURES,  # or specify the number of features, e.g., 10
-        direction=SFS_DIRECTION,
-        scoring=FORWARD_SELECTION_SCORING,  # or 'f1' for f1-score, etc.
-        cv=FORWARD_SELECTION_CV,  # Number of folds in cross-validation
-        n_jobs=-1  # Use all available cores
-    )
-
-    # Fit the SFS to the data
-    sfs.fit(X, y)
-
-    # Get the selected feature indices
-    selected_features_sfs = X.columns[sfs.get_support()]
-    X = X[selected_features_sfs]
-    validation_df = validation_df[selected_features_sfs]
-
-    print(f'Optimal number of features: {sfs.n_features_in_}')
-
-    
-
-'''Saving processed data'''
-train_df = pd.concat([X, y], axis=1)
-train_df.to_csv(TRAIN_DATA_PROCESSED_PATH, index=False)
-validation_df.to_csv(VALIDATION_DATA_PROCESSED_PATH, index=False)
+    mlflow.end_run()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,19 +1,18 @@
 import yaml
 import pandas as pd
+import mlflow
+import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold
 import joblib
 
-
-
 '''Loading parameters'''
-paths  =yaml.safe_load(open('./params.yaml'))['paths']
-params  =yaml.safe_load(open('./params.yaml'))['model_building']
+paths = yaml.safe_load(open('./params.yaml'))['paths']
+params = yaml.safe_load(open('./params.yaml'))['model_building']
 
 TRAIN_DATA_PATH = paths['train_data_processed_path']
 VALIDATION_DATA_PATH = paths['test_data_processed_path']
@@ -22,114 +21,126 @@ MODEL_PATH = paths['model_path']
 MODEL = params['model']
 TEST_SIZE = params['test_size']
 RANDOM_STATE = params['random_state']
-FEATURE_SELECTION_TECHNIQUE  =yaml.safe_load(open('./params.yaml'))['feature_engineering']['selection_technique']
+FEATURE_SELECTION_TECHNIQUE = yaml.safe_load(open('./params.yaml'))['feature_engineering']['selection_technique']
 HYPERPARAMETER_TUNING = params['tuning']
 THRESHOLD = params['threshold']
 
 train_df = pd.read_csv(TRAIN_DATA_PATH)
 validation_df = pd.read_csv(VALIDATION_DATA_PATH)
 
-
 # Splitting the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(train_df.drop(columns=['Target']),train_df['Target'] ,
+X_train, X_test, y_train, y_test = train_test_split(train_df.drop(columns=['Target']), train_df['Target'],
                                                     test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=train_df['Target'])
 
+# Start an MLflow run
+with mlflow.start_run():
 
+    if MODEL == 'XGB':
+        if not HYPERPARAMETER_TUNING:
+            # Initialize the XGBoost classifier
+            model = XGBClassifier(eval_metric='logloss', random_state=42)
+            # Train the model
+            model.fit(X_train, y_train)
 
-if MODEL == 'XGB':
-    if HYPERPARAMETER_TUNING ==False:
-        # Initialize the XGBoost classifier
-        model = XGBClassifier(eval_metric='logloss', random_state=42)
-        # Train the model
-        model.fit(X_train, y_train)
-        
-        threshold = 0.3
-        y_pred_proba = model.predict_proba(X_test)[:, 1]
-        y_pred = (y_pred_proba >= threshold).astype(int)
+            threshold = 0.3
+            y_pred_proba = model.predict_proba(X_test)[:, 1]
+            y_pred = (y_pred_proba >= threshold).astype(int)
 
-        # Calculate F1-score
-        f1 = f1_score(y_test, y_pred, average='macro')
-        print(f'F1-Score on Validation Set: {f1:.4f}')
-        
-            # Compute the confusion matrix
-        cm = confusion_matrix(y_test, y_pred)
-        # Plot the confusion matrix using seaborn's heatmap
-        plt.figure(figsize=(8, 6))
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
-        plt.xlabel('Predicted Labels')
-        plt.ylabel('True Labels')
-        plt.title('Confusion Matrix')
-        plt.savefig(f'./reports/figures/{MODEL}_{FEATURE_SELECTION_TECHNIQUE}')
-    
-    if HYPERPARAMETER_TUNING == True :
+            # Calculate F1-score
+            f1 = f1_score(y_test, y_pred, average='macro')
+            print(f'F1-Score on Validation Set: {f1:.4f}')
 
-        # Define the parameter grid
-        param_grid = {
-            'n_estimators': [100, 200, 300, 500],
-            'max_depth': [3, 5, 7, 9],
-            'learning_rate': [0.01, 0.05, 0.1, 0.2],
-            'subsample': [0.6, 0.8, 1.0],
-            'colsample_bytree': [0.6, 0.8, 1.0],
-            'gamma': [0, 0.1, 0.2, 0.3],
-            'min_child_weight': [1, 3, 5]
-        }
+            # Log parameters and metrics
+            mlflow.log_param("model_type", MODEL)
+            mlflow.log_param("threshold", threshold)
+            mlflow.log_metric("f1_score", f1)
 
-        # Initialize XGBoost classifier with GPU support
-        xgb_model = XGBClassifier(
-            tree_method='gpu_hist',   # Use GPU acceleration
-            predictor='gpu_predictor',
-            objective='binary:logistic',
-            eval_metric='logloss',
-            random_state=42
-        )
+            # Compute and plot the confusion matrix
+            cm = confusion_matrix(y_test, y_pred)
+            plt.figure(figsize=(8, 6))
+            sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
+            plt.xlabel('Predicted Labels')
+            plt.ylabel('True Labels')
+            plt.title('Confusion Matrix')
+            cm_path = f'./reports/figures/{MODEL}_{FEATURE_SELECTION_TECHNIQUE}.png'
+            plt.savefig(cm_path)
+            mlflow.log_artifact(cm_path)
 
-        # Define StratifiedKFold cross-validator
-        cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+        else:
+            # Define the parameter grid
+            param_grid = {
+                'n_estimators': [100, 200, 300, 500],
+                'max_depth': [3, 5, 7, 9],
+                'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                'subsample': [0.6, 0.8, 1.0],
+                'colsample_bytree': [0.6, 0.8, 1.0],
+                'gamma': [0, 0.1, 0.2, 0.3],
+                'min_child_weight': [1, 3, 5]
+            }
 
-        # Define the RandomizedSearchCV with f1 score as the scoring metric
-        random_search = RandomizedSearchCV(
-            estimator=xgb_model,
-            param_distributions=param_grid,
-            scoring='f1_macro',
-            n_iter=50,  # Number of parameter settings that are sampled
-            cv=cv,
-            verbose=1,
-            random_state=42,
-            n_jobs=-1  # Use all available cores
-        )
+            # Initialize XGBoost classifier with GPU support
+            xgb_model = XGBClassifier(
+                tree_method='gpu_hist',   # Use GPU acceleration
+                predictor='gpu_predictor',
+                objective='binary:logistic',
+                eval_metric='logloss',
+                random_state=42
+            )
 
-        # Fit the model to the training data
-        random_search.fit(X_train, y_train)
-        
-        # After running the RandomizedSearchCV and obtaining the best model
-        model = random_search.best_estimator_
+            # Define StratifiedKFold cross-validator
+            cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-        # Adjusting the decision threshold
-        ## threshold for transaction to be non fradulent i.e. if threshold = 1 all transactions are non fradulent
-        threshold = THRESHOLD
-        y_pred_proba = model.predict_proba(X_test)[:, 1]
-        y_pred_threshold = (y_pred_proba >= threshold).astype(int)
+            # Define the RandomizedSearchCV with f1 score as the scoring metric
+            random_search = RandomizedSearchCV(
+                estimator=xgb_model,
+                param_distributions=param_grid,
+                scoring='f1_macro',
+                n_iter=50,  # Number of parameter settings that are sampled
+                cv=cv,
+                verbose=1,
+                random_state=42,
+                n_jobs=-1  # Use all available cores
+            )
 
-        # Calculate F1-score
-        f1 = f1_score(y_test, y_pred_threshold, average='macro')
-        print(f'F1-Score on Validation Set: {f1:.4f}')
+            # Fit the model to the training data
+            random_search.fit(X_train, y_train)
 
-        cm = confusion_matrix(y_test, y_pred)
-        # Plot the confusion matrix using seaborn's heatmap
-        plt.figure(figsize=(8, 6))
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
-        plt.xlabel('Predicted Labels')
-        plt.ylabel('True Labels')
-        plt.title('Confusion Matrix')
-        plt.savefig(f'./reports/figures/{MODEL}_{FEATURE_SELECTION_TECHNIQUE}')
-    
-    
-elif MODEL == 'ANOMOLY':
-    pass
+            # After running the RandomizedSearchCV and obtaining the best model
+            model = random_search.best_estimator_
 
+            # Adjusting the decision threshold
+            threshold = THRESHOLD
+            y_pred_proba = model.predict_proba(X_test)[:, 1]
+            y_pred_threshold = (y_pred_proba >= threshold).astype(int)
 
+            # Calculate F1-score
+            f1 = f1_score(y_test, y_pred_threshold, average='macro')
+            print(f'F1-Score on Validation Set: {f1:.4f}')
 
-# Save the model
-joblib.dump(model, MODEL_PATH)
+            # Log parameters and metrics
+            mlflow.log_param("model_type", MODEL)
+            mlflow.log_param("threshold", threshold)
+            mlflow.log_param("hyperparameter_tuning", HYPERPARAMETER_TUNING)
+            mlflow.log_metrics(random_search.best_params_)
+            mlflow.log_metric("f1_score", f1)
 
+            # Compute and plot the confusion matrix
+            cm = confusion_matrix(y_test, y_pred_threshold)
+            plt.figure(figsize=(8, 6))
+            sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
+            plt.xlabel('Predicted Labels')
+            plt.ylabel('True Labels')
+            plt.title('Confusion Matrix')
+            cm_path = f'./reports/figures/{MODEL}_{FEATURE_SELECTION_TECHNIQUE}.png'
+            plt.savefig(cm_path)
+            mlflow.log_artifact(cm_path)
 
+    elif MODEL == 'ANOMALY':
+        pass
+
+    # Save and log the model
+    joblib.dump(model, MODEL_PATH)
+    mlflow.log_artifact(MODEL_PATH)
+    mlflow.sklearn.log_model(model, "model")
+
+    print("Model saved and logged successfully.")
