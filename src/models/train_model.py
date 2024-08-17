3cd7458c9cdabe1b526c0e53659cb00654b3fb1a
sarthakg004 <saarthak.gupta.mec22@itbhu.ac.in>
2024-08-17 18:56:16 +0530

    EXPERIMENT 2

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -7,10 +7,12 @@ from xgboost import XGBClassifier
 from sklearn.metrics import f1_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, RandomizedSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import joblib
-import dagshub 
+import dagshub
+from tqdm import tqdm
+from functools import partial
 
 dagshub.init(repo_owner='sarthakg004', repo_name='fraud_detection', mlflow=True)
 mlflow.set_tracking_uri("https://dagshub.com/sarthakg004/fraud_detection.mlflow")
@@ -38,6 +40,7 @@ with mlflow.start_run(run_name='model_building'):
     HYPERPARAMETER_TUNING = params['tuning']
     THRESHOLD = params['threshold']
     XGB_EVAL_METRIC = params['XGB_eval_metric']
+    DEVICE = yaml.safe_load(open('./params.yaml'))['feature_engineering']['forward_selection_device']
 
     
 
@@ -48,18 +51,21 @@ with mlflow.start_run(run_name='model_building'):
 
 
     
-    
-    
-    
     train_df = pd.read_csv(TRAIN_DATA_PATH)
     validation_df = pd.read_csv(VALIDATION_DATA_PATH)
+    
+    
 
     mlflow.log_input(mlflow.data.from_pandas(train_df), "preprocessed_data")
     mlflow.log_input(mlflow.data.from_pandas(validation_df), "validation_data")
+    mlflow.log_artifact(TRAIN_DATA_PATH, "preprocessed_data")
+    mlflow.log_artifact(VALIDATION_DATA_PATH, "validation_data")
     
     # Splitting the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(train_df.drop(columns=['Target']), train_df['Target'],
                                                         test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=train_df['Target'])
+    
+    
 
     if MODEL == 'XGB':
         if not HYPERPARAMETER_TUNING:
@@ -120,10 +126,10 @@ with mlflow.start_run(run_name='model_building'):
 
             # Initialize XGBoost classifier with GPU support
             xgb_model = XGBClassifier(
-                tree_method='gpu_hist',   # Use GPU acceleration
-                predictor='gpu_predictor',
+                tree_method='hist',   # Use GPU acceleration
+                device= DEVICE,
                 objective='binary:logistic',
-                eval_metric= XGB_EVAL_METRIC,
+                eval_metric=XGB_EVAL_METRIC,
                 random_state=42
             )
 
@@ -131,22 +137,22 @@ with mlflow.start_run(run_name='model_building'):
             cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
             # Define the RandomizedSearchCV with f1 score as the scoring metric
-            grid_search = GridSearchCV(
+            random_search = RandomizedSearchCV(
                 estimator=xgb_model,
                 param_distributions=param_grid,
                 scoring='f1_macro',
-                n_iter=50,  # Number of parameter settings that are sampled
+                n_iter=150,  # Number of parameter settings that are sampled
                 cv=cv,
-                verbose=2,
+                verbose=1,
                 random_state=42,
                 n_jobs=-1  # Use all available cores
             )
 
             # Fit the model to the training data
-            grid_search.fit(X_train, y_train)
+            random_search.fit(X_train, y_train)
 
-            # After running the RandomizedSearchCV and obtaining the best model
-            model = grid_search.best_estimator_
+            # After running the GridSearchCV and obtaining the best model
+            model = random_search.best_estimator_
 
             # Adjusting the decision threshold
             threshold = THRESHOLD
@@ -175,16 +181,15 @@ with mlflow.start_run(run_name='model_building'):
             mlflow.log_metric("f1_score", f1)
             mlflow.log_metric("roc_auc_score", roc_auc)
             
-            for i in range(len(grid_search.cv_results_['params'])):
+            for i in range(len(random_search.cv_results_['params'])):
                 with mlflow.start_run(nested=True):
                     # Log Hyperparameters
-                    mlflow.log_params(grid_search.cv_results_['params'][i])
-                    mlflow.log_metric('mean_f1_test_score', grid_search.cv_results_['mean_test_score'][i])
+                    mlflow.log_params(random_search.cv_results_['params'][i])
+                    mlflow.log_metric('mean_f1_test_score', random_search.cv_results_['mean_test_score'][i])
                     
             
-            mlflow.log_params(grid_search.best_params_)
+            mlflow.log_params(model.get_params())
             
-
             # Compute and plot the confusion matrix
             cm = confusion_matrix(y_test, y_pred)
             plt.figure(figsize=(8, 6))
@@ -192,9 +197,14 @@ with mlflow.start_run(run_name='model_building'):
             plt.xlabel('Predicted Labels')
             plt.ylabel('True Labels')
             plt.title('Confusion Matrix')
-            cm_path = f'./reports/figures/{MODEL}_{FEATURE_SELECTION_TECHNIQUE}.png'
+            cm_path = f'./reports/figures/confusion_matrix.png'
             plt.savefig(cm_path)
             mlflow.log_artifact(cm_path)
+        
+        # Save the trained model
+        joblib.dump(model, MODEL_PATH)
+        mlflow.log_artifact(MODEL_PATH)
+
 
     elif MODEL == 'ANOMALY':
         pass
